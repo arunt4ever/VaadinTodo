b02f5ba12f8b5d71b93321bbbccab59a4497b88c
Arun Thangavel <arunt.4ever@gmail.com>
2022-06-08 10:45:46 -0700

    Initial Commit

---
+++ src/main/java/com/arunzlair/vaadintodo/VaadinTodoApplication.java
@@ -0,0 +1,13 @@
+package com.arunzlair.vaadintodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaadinTodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VaadinTodoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/arunzlair/vaadintodo/VaadinTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arunzlair.vaadintodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaadinTodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
